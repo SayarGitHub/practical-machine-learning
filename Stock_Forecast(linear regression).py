25a29b981ec7460a6ccdeca047409e86cf8b048f
SayarGitHub <52607268+SayarGitHub@users.noreply.github.com>
2020-12-16 01:25:08 +0530

    Updated to work from saved stock data

--- Stock_Forecast(linear regression).py
+++ Stock_Forecast(linear regression).py
@@ -6,11 +6,12 @@ from sklearn import model_selection
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
+import pickle
 
 style.use("ggplot")
 
-df = quandl.get("WIKI/GOOGL")
-df.to_csv("Google_Stocks.csv")
+#df = quandl.get("WIKI/GOOGL")
+#df.to_csv("Google_Stocks.csv")
 df = pd.read_csv("Google_Stocks.csv", index_col="Date")
 
 col = df.columns
@@ -46,9 +47,9 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_siz
 
 clf = LinearRegression(n_jobs=-1) #n_jobs=-1 =>use all processing power for other values it represents parallel threads
 
-clf.fit(X_train,y_train)
-with open("linearregression.pickle","wb") as f:
-    pickle.dump(clf,f)
+#clf.fit(X_train,y_train)
+#with open("linearregression.pickle","wb") as f:
+    #pickle.dump(clf,f)
 pickle_in = open("linearregression.pickle","rb")
 clf = pickle.load(pickle_in)
 
@@ -61,7 +62,7 @@ print(forecast_set)
 df["Forecast"] = np.nan
 
 last_date = df.iloc[-1].name
-last_unix = last_date.timestamp()
+last_unix = datetime.datetime.strptime(last_date,"%Y-%m-%d").timestamp()
 one_day = 86400
 next_unix = last_unix+one_day
 
